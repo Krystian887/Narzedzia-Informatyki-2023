973c59cc1cd041024e486f7d1171533c40b2901f
Krystian887 <krystian.lukasik@o2.pl>
2023-06-24 16:34:33 +0200

    Dodanie dwoch plików
    Dodanie dwoch plików z kodem w programie java

---
+++ Przykladowy projekt2/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Greetings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj swoje imię: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Witaj, " + name + "! Miło cię poznać.");
+    }
+}
